6c3fb4c18394b634c85ae0527f488eebca84e34a
Luis B <luis.beroiza@gmail.com>
2023-05-26 14:55:00 +0200

    Agrega Vista con Lista de Clientes y Datos de Pruebas
    el archivo import.sql debe ir en la carpeta resources.
    
    de esta forma el servidor importará automaticamente
    los datos en la base de datos al momento de levantar el servicio.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+/* Populate Tables */
+
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (1, 'Luis', 'Beroiza', 'luis.beroiza@email.com', '2023-05-26', '2023-05-26');
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (2, 'Cata', 'Salazar', 'pequitas@email.com','2023-05-26', '2023-05-26');
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (3, 'Monica', 'Godoy', 'ojos-verdes@email.com','2023-05-26', '2023-05-26');
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (4, 'Lucia', 'Mora', 'rubia@email.com','2023-05-26', '2023-05-26');
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (5, 'Lili', 'Fernandez', 'lili.fer@email.com', '2023-05-26', '2023-05-26');
+INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (6, 'Elisabeth', 'Smith', 'es-gbr@email.com', '2023-05-26', '2023-05-26');
