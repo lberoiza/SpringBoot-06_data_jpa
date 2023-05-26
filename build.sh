ebd17aec269306a23cdb561f5e7d39f8662b5a01
Luis B <luis.beroiza@gmail.com>
2023-05-26 13:53:11 +0200

    Creando Proyecto Spring con JPA-Hibernate
    JPA (Java Persistence Api) explica el standard de como se deben definir los ORM
    (Object-Relational Mapping) como EclipseLink o Hibernate.
    
    En este proyecto usaremos Hibernate, debido a que Spring Trabaja con el de manera nativa.
    Y como motores de base de datos usaremos H2 (Base de datos en memoria)
    Y despues migraremos a MySQL.

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# este comando crea el jar con el projecto
+# el archivo está dentro de la carpeta /target
+# el nombre es "target/spring-boot-web-0.0.1-SNAPSHOT.jar"
+./mvnw package
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# para correr el jar creado
+
+java -jar target/spring-boot-web*.jar
