48bcefcea17ad23a5ede8d3171ddc92ae0080919
Luis B <luis.beroiza@adesso.de>
2023-05-31 10:28:57 +0200

    Agrega Propiedades de Conexion MySQL y Modifica Script
    El Import-Script no necesita que se reinicien los indices autogenerados
    como con H2, por lo que se comenta esa linea, ademas no es compatible con MySql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,13 @@
 /* Populate Tables */
 
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (1, 'Luis', 'Beroiza', 'luis.beroiza@email.com', '2023-05-26', '2023-05-26');
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (2, 'Cata', 'Salazar', 'pequitas@email.com','2023-05-26', '2023-05-26');
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (3, 'Monica', 'Godoy', 'ojos-verdes@email.com','2023-05-26', '2023-05-26');
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (4, 'Lucia', 'Mora', 'rubia@email.com','2023-05-26', '2023-05-26');
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (5, 'Lili', 'Fernandez', 'lili.fer@email.com', '2023-05-26', '2023-05-26');
-INSERT INTO Clients (id, name, surname, email, created_at, updated_at) VALUES (6, 'Elisabeth', 'Smith', 'es-gbr@email.com', '2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (1, 'Luis', 'Beroiza', 'luis.beroiza@email.com', '2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (2, 'Cata', 'Salazar', 'pequitas@email.com','2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (3, 'Monica', 'Godoy', 'ojos-verdes@email.com','2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (4, 'Lucia', 'Mora', 'rubia@email.com','2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (5, 'Lili', 'Fernandez', 'lili.fer@email.com', '2023-05-26', '2023-05-26');
+INSERT INTO clients (id, name, surname, email, created_at, updated_at) VALUES (6, 'Elisabeth', 'Smith', 'es-gbr@email.com', '2023-05-26', '2023-05-26');
 
 /* Genera el ID autoincremental a partir del id maximo de la tabla */
-ALTER TABLE Clients ALTER COLUMN id RESTART WITH (SELECT MAX(id) + 1 FROM Clients);
+/* ALTER TABLE clients ALTER COLUMN id RESTART WITH (SELECT MAX(id) + 1 FROM clients); */
+
+
