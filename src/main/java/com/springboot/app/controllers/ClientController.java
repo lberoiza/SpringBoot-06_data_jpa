e94d3b860733a8184265324ce19f8a5c7a55d403
Luis B <luis.beroiza@adesso.de>
2023-06-20 10:26:43 +0200

    Agrega Mensajes de exito o fracaso que funcionan mediante una redireccion.
    Para eso se usa la clase RedirectAttributes.
    Se pueden mandar variables atraves de la url usando addAtributes o temporales en la sesion
    usando addFlashAttribute

--- src/main/java/com/springboot/app/controllers/ClientController.java
+++ src/main/java/com/springboot/app/controllers/ClientController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springboot.app.models.entity.Client;
 import com.springboot.app.services.IClientService;
@@ -42,21 +43,26 @@ public class ClientController {
   }
 
   @RequestMapping(value = "/form", method = RequestMethod.POST)
-  public String createClient(@Valid Client client, BindingResult result, Model model, SessionStatus status) {
+  public String createClient(@Valid Client client, BindingResult result, Model model, RedirectAttributes flash,
+      SessionStatus status) {
 
     if (result.hasErrors()) {
       model.addAttribute("title", "Create Client");
+      model.addAttribute("error", "Please full all fields.");
       return "client/form";
     }
 
+    String operationTypString = client.hasValidId() ? "updated" : "created";
+
     clientService.saveOrUpdate(client);
     status.setComplete();
+    flash.addFlashAttribute("sucess", String.format("The Client was successful %s", operationTypString));
     return "redirect:/client/list";
 
   }
 
   @RequestMapping(value = "/form/{id}", method = RequestMethod.GET)
-  public String editClient(@PathVariable Long id, Model model) {
+  public String editClient(@PathVariable Long id, Model model, RedirectAttributes flash) {
     Optional<Client> result = clientService.findById(id);
 
     if (result.isPresent()) {
@@ -65,12 +71,14 @@ public class ClientController {
       return "client/form";
     }
 
+    flash.addFlashAttribute("error", "The Client doesnt exist in der Database.");
     return "redirect:/client/list";
   }
 
   @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-  public String editClient(@PathVariable Long id) {
+  public String deleteClient(@PathVariable Long id, RedirectAttributes flash) {
     clientService.delete(id);
+    flash.addFlashAttribute("sucess", "The Client was successful deleted.");
     return "redirect:/client/list";
   }
 
