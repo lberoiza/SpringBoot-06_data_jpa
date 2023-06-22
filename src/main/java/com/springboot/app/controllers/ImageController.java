bd9767ca5f80dd9dc7a8b196b914f870ffb3b5d7
Luis B <luis.beroiza@adesso.de>
2023-06-22 18:32:53 +0200

    Retorna las Imagenes como Respuesta HTML usando controlador
    Controlador para cargar imagenes. es una forma alternativa a la de definir un recurso.

---
+++ src/main/java/com/springboot/app/controllers/ImageController.java
@@ -0,0 +1,50 @@
+package com.springboot.app.controllers;
+
+import com.springboot.app.services.IUploadFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+
+@Controller
+public class ImageController {
+
+  @Autowired
+  private IUploadFileService uploadFileService;
+
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+  @GetMapping(value = "/images/{filename:.+}")
+  public ResponseEntity<Resource> showImage(@PathVariable String filename) {
+    Path imagePath = uploadFileService.getRootPathImages().resolve(filename).toAbsolutePath();
+    logger.info("Pathimage: " + imagePath);
+
+    Resource resource = null;
+    String resourceFilename = "";
+    try {
+      resource = new UrlResource(imagePath.toUri());
+      resourceFilename = resource.getFilename();
+      if (!resource.exists() || !resource.isReadable()) {
+        String error = String.format("The Image can't be loaded: '%s'", imagePath);
+        logger.error(error);
+        throw new RuntimeException(error);
+      }
+
+    } catch (MalformedURLException mfex) {
+      logger.error(mfex.toString());
+    }
+
+    String header = String.format("attachment; filename=\"%s\"", resourceFilename);
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, header).body(resource);
+  }
+}
