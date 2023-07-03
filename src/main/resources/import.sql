19ca5eb1dc0f2556abb150c45f820908c31a6405
Luis B <luis.beroiza@adesso.de>
2023-07-03 21:14:52 +0200

    Agrega Datos de prueba para la tabla Products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 /* Populate Tables */
 
+/* TABLE: Clients */
 INSERT INTO clients (id, name, surname, email, created_at, updated_at, image) VALUES (1, 'Luis', 'Beroiza', 'luis.beroiza@email.com', '2023-05-26', '2023-05-26', '');
 INSERT INTO clients (id, name, surname, email, created_at, updated_at, image) VALUES (2, 'Cata', 'Salazar', 'pequitas@email.com','2023-05-26', '2023-05-26', '');
 INSERT INTO clients (id, name, surname, email, created_at, updated_at, image) VALUES (3, 'Monica', 'Godoy', 'ojos-verdes@email.com','2023-05-26', '2023-05-26', '');
@@ -11,3 +12,11 @@ INSERT INTO clients (id, name, surname, email, created_at, updated_at, image) VA
 /* ALTER TABLE clients ALTER COLUMN id RESTART WITH (SELECT MAX(id) + 1 FROM clients); */
 
 
+/* TABLE: Products */
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (750, NOW(), NOW(), '35 inch Television wide screen', 'Panasonic LCD Screen');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (350, NOW(), NOW(), 'Digital Camera Sony DSC-W320B', 'Sony Camera');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (300, NOW(), NOW(), 'Apple Ipod Shuffle', 'Ipod');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (1500, NOW(), NOW(), 'Notebook Sony Vaio 15 inch', 'Sony Vaio');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (279.89, NOW(), NOW(), 'Hewlett Packard Multiprinter', ' Printer HP');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (1359.89, NOW(), NOW(), 'Bicycle Bianchi 17 inch wheels', 'Bianchi Bicycle');
+INSERT INTO products (price, created_at, updated_at, description, name) VALUES  (435, NOW(), NOW(), 'Desk for office', 'Mica Desk');
