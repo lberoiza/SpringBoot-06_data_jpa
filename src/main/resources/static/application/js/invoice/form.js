4320f8a7e2e269e37e65fbbb38e7cc23e5fe2d19
Luis B <luis.beroiza@adesso.de>
2023-07-05 13:35:51 +0200

    Agrega funcionalidad en javascript para agregar productos a la factura

--- src/main/resources/static/application/js/invoice/form.js
+++ src/main/resources/static/application/js/invoice/form.js
@@ -2,26 +2,37 @@ class InvoiceForm {
 
   FETCH_URL_PRODUCT_LIST_BY_NAME = "/product/find_by_name/";
 
+  FORM_INVOICE_ID = "form-new-invoice";
   INPUT_PRODUCT_ID = "product";
   DATALIST_PRODUCT_ID = "datalist-product";
+  BUTTON_ADD_PRODUCT = "add-product";
 
   constructor() {
-    this.selectedProducts = [];
+    this.selectedProductMap = new Map();
     this.currentProductDataList = [];
     this.debounce = new Debounce(500, () => this.getProducts());
+    this.initializeComponents();
     this.initializeListeners()
 
   }
 
+  initializeComponents() {
+    this.form = document.getElementById(this.FORM_INVOICE_ID);
+    this.inputSearchProduct = document.getElementById(this.INPUT_PRODUCT_ID);
+    this.datalistProduct = document.getElementById(this.DATALIST_PRODUCT_ID);
+    this.buttonAddProduct = document.getElementById(this.BUTTON_ADD_PRODUCT);
+  }
+
 
   initializeListeners() {
-    const inputProduct = document.getElementById(this.INPUT_PRODUCT_ID);
-    inputProduct.addEventListener('input', () => this.debounce.start());
+    this.form.addEventListener('submit', (e) => this.submitInvoice(e));
+    this.inputSearchProduct.addEventListener('input', () => this.debounce.start());
+    this.buttonAddProduct.addEventListener('click', (e) => this.addProduct())
   }
 
 
   getProductSearchTerm() {
-    return document.getElementById(this.INPUT_PRODUCT_ID).value.trim();
+    return this.inputSearchProduct.value.trim();
   }
 
 
@@ -49,14 +60,51 @@ class InvoiceForm {
 
   loadDatalistProduct(result = []) {
     this.currentProductDataList = result;
-    const datalistProduct = document.getElementById(this.DATALIST_PRODUCT_ID);
-    datalistProduct.innerHTML = "";
+    this.datalistProduct.innerHTML = "";
     result.forEach(product => {
       const optionProduct = document.createElement("option");
       optionProduct.value = product.name;
-      datalistProduct.appendChild(optionProduct);
+      this.datalistProduct.appendChild(optionProduct);
     });
   }
+
+
+  addProduct() {
+    const selectedProduct = this.getSelectedProduct();
+    if (selectedProduct) {
+      const productId = selectedProduct.id
+      let invoiceItem = {};
+      if (this.selectedProductMap.has(productId)) {
+        invoiceItem = this.selectedProductMap.get(productId)
+        invoiceItem.quantity += 1;
+      } else {
+        invoiceItem = {
+          quantity: 1,
+          product: selectedProduct
+        }
+        this.selectedProductMap.set(productId, invoiceItem);
+      }
+      Alert.showSuccess(`Product ${invoiceItem.product.name} added.`)
+      this.clearDataListAndInput();
+    } else {
+      Alert.showWarning(`Product not in the list.`);
+    }
+    this.inputSearchProduct.focus();
+  }
+
+
+  clearDataListAndInput() {
+    // this.datalistProduct.innerHTML = "";
+    this.inputSearchProduct.value = "";
+  }
+
+  submitInvoice(event) {
+    event.preventDefault();
+    console.log("submit");
+    console.log("list of products", this.selectedProductMap);
+
+  }
+
 
 }
 
