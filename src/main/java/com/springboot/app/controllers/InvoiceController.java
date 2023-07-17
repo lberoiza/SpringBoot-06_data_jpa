f79ed88343f2b68a3a02b69e66867e906b73ef90
Luis B <luis.beroiza@adesso.de>
2023-07-17 15:10:18 +0200

    Agrega Metodo para guardar factura

--- src/main/java/com/springboot/app/controllers/InvoiceController.java
+++ src/main/java/com/springboot/app/controllers/InvoiceController.java
@@ -2,23 +2,45 @@ package com.springboot.app.controllers;
 
 import com.springboot.app.models.entity.Client;
 import com.springboot.app.models.entity.Invoice;
+import com.springboot.app.models.entity.InvoiceItem;
+import com.springboot.app.models.entity.Product;
 import com.springboot.app.services.IClientService;
+import com.springboot.app.services.IInvoiceService;
+import com.springboot.app.services.IProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/invoice")
+@SessionAttributes("invoice")
 public class InvoiceController {
 
-  @Autowired
-  private IClientService clientService;
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+  private final IClientService clientService;
+
+  private final IProductService productService;
+
+  private final IInvoiceService invoiceService;
+
+
+  public InvoiceController(@Autowired IClientService clientService,
+                           @Autowired IProductService productService,
+                           @Autowired IInvoiceService invoiceService){
+    this.clientService = clientService;
+    this.productService = productService;
+    this.invoiceService = invoiceService;
+  }
+
 
   @GetMapping("/form/{clientId}")
   public String createInvoice(@PathVariable(value = "clientId") Long clientId,
@@ -42,5 +64,46 @@ public class InvoiceController {
 
     return "invoice/form";
   }
+
+
+  @PostMapping("/form")
+  public String saveInvoice(Invoice invoice,
+                            @RequestParam(name = "product_id[]", required = false) Long[] productIds,
+                            @RequestParam(name = "product_quantity[]", required = false) Integer[] productQuantities,
+                            RedirectAttributes flash,
+                            SessionStatus status){
+
+    for(int i =0; i < productIds.length; i++){
+      Long productId = productIds[i];
+      Integer quantity = productQuantities[i];
+      Product product = null;
+      String productName = "product not found";
+
+      Optional<Product> optionalProduct = productService.findById(productIds[i]);
+      if(optionalProduct.isPresent()){
+        product = optionalProduct.get();
+        productName = product.getName();
+      }
+
+      InvoiceItem invoiceItem = new InvoiceItem();
+      invoiceItem.setProduct(product);
+      invoiceItem.setQuantity(quantity);
+      invoice.addInvoiceItem(invoiceItem);
+      String infoStr = String.format("Product (%d) \"%s\" with quantity: %d added to invoice (%d)", productId, productName, quantity, invoice.getId());
+      logger.info(infoStr);
+    }
+
+
+    this.invoiceService.saveInvoice(invoice);
+    status.setComplete();
+
+    String flashString = String.format("The Invoice (%s) was successfully created for %s", invoice.getId(), invoice.getClientFullName());
+    flash.addFlashAttribute("success", flashString);
+
+    return "redirect:/client/" + invoice.getClientId();
+  }
+
+
+
 
 }
