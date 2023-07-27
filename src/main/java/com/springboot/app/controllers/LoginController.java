c4108d2c651e38b4d72312200d853dcdcf04ed7b
Luis B <luis.beroiza@adesso.de>
2023-07-27 13:11:24 +0200

    Controla errores de logueo
    Si hay errores serán mostrados en la pantalla de login

--- src/main/java/com/springboot/app/controllers/LoginController.java
+++ src/main/java/com/springboot/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.springboot.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,11 +12,19 @@ import java.security.Principal;
 public class LoginController {
 
   @GetMapping("/login")
-  public String login(Model model, Principal principal, RedirectAttributes flash){
+  public String login(
+      @RequestParam(value = "error", required = false) String error,
+      Model model,
+      Principal principal, RedirectAttributes flash){
     if(principal != null ) {
       String info = "You are already logged";
       flash.addFlashAttribute("info", info);
-      return "redirect:/";
+      return "redirect:" + MainControler.START_POINT;
+    }
+
+    if(error != null){
+      error = "Your Username or Password are incorrect. Please try again.";
+      model.addAttribute("error", error);
     }
 
     return "login/login_page";
