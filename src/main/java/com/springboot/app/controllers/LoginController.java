a3a5a2c9e651eeefa7124c2de1f47007987c7631
Luis B <luis.beroiza@adesso.de>
2023-07-28 12:45:02 +0200

    Agrega Link para Iniciar sesión y cerrar sesión.
    para eso se usan las secciones de Spring Security en Thymeleaf

--- src/main/java/com/springboot/app/controllers/LoginController.java
+++ src/main/java/com/springboot/app/controllers/LoginController.java
@@ -14,6 +14,7 @@ public class LoginController {
   @GetMapping("/login")
   public String login(
       @RequestParam(value = "error", required = false) String error,
+      @RequestParam(value = "logout", required = false) String logout,
       Model model,
       Principal principal, RedirectAttributes flash){
     if(principal != null ) {
@@ -25,6 +26,11 @@ public class LoginController {
     if(error != null){
       error = "Your Username or Password are incorrect. Please try again.";
       model.addAttribute("error", error);
+    }
+
+    if(logout != null){
+      logout = "Your are successfully disconnected";
+      model.addAttribute("success", logout);
     }
 
     return "login/login_page";
