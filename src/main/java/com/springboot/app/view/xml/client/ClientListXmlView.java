d10ee8a8a1d4c95f8ce3214bffe2a4a84dc18843
Luis B <luis.beroiza@adesso.de>
2023-08-22 19:57:45 +0200

    Agrega la Vista para exportar en Xml y el boton en la vista Thymeleaf

---
+++ src/main/java/com/springboot/app/view/xml/client/ClientListXmlView.java
@@ -0,0 +1,41 @@
+package com.springboot.app.view.xml.client;
+
+import com.springboot.app.exporters.xml.wrappers.ClientList;
+import com.springboot.app.models.entity.Client;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.xml.MarshallingView;
+
+import java.util.Map;
+
+@Component("client/show_list.xml")
+public class ClientListXmlView extends MarshallingView {
+
+  @Autowired
+  public ClientListXmlView(Jaxb2Marshaller marshaller) {
+    super(marshaller);
+  }
+
+  @Override
+  protected void renderMergedOutputModel(Map<String, Object> model,
+                                         HttpServletRequest request,
+                                         HttpServletResponse response) throws Exception {
+
+    @SuppressWarnings("unchecked")
+    Page<Client> clients = (Page<Client>) model.get("clients");
+    model.clear();
+    model.put("clientList", new ClientList(clients.getContent()));
+
+//    model.remove("title");
+//    model.remove("pageRender");
+//    model.remove("clients");
+
+
+
+    super.renderMergedOutputModel(model, request, response);
+  }
+}
