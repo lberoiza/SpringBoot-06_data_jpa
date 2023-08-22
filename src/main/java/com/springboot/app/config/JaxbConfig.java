9e4e1dc82af430edf3a2c9c5baeac1f9aefd63d6
Luis B <luis.beroiza@adesso.de>
2023-08-22 19:52:44 +0200

    Configuracion para exportar en XML

---
+++ src/main/java/com/springboot/app/config/JaxbConfig.java
@@ -0,0 +1,17 @@
+package com.springboot.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class JaxbConfig {
+
+  @Bean
+  public Jaxb2Marshaller jaxb2Marshaller() {
+    Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    marshaller.setClassesToBeBound(new Class[]{com.springboot.app.exporters.xml.wrappers.ClientList.class});
+    return marshaller;
+  }
+
+}
