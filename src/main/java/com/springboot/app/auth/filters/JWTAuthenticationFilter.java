5be42036100874688c3b185757ba1a0ae9982eab
Luis B <luis.beroiza@adesso.de>
2023-08-23 16:50:39 +0200

    Genera JWT despues de que la autenticacion sea correcta

--- src/main/java/com/springboot/app/auth/filters/JWTAuthenticationFilter.java
+++ src/main/java/com/springboot/app/auth/filters/JWTAuthenticationFilter.java
@@ -1,15 +1,29 @@
 package com.springboot.app.auth.filters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
   private final AuthenticationManager authenticationManager;
 
@@ -34,5 +48,28 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 
     return this.authenticationManager.authenticate(authToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+    User user = (User) authResult.getPrincipal();
+
+    String token = Jwts.builder()
+        .setSubject(user.getUsername())
+        .signWith(SECRET_KEY)
+        .compact();
+
+    response.addHeader("Authorization", String.format("Bearer %s", token));
+
+    Map<String, Object> body = new HashMap<>();
+    body.put("token", token);
+    body.put("user", user);
+    body.put("message", String.format("Hi, %s. You has initialized session successfully", user.getUsername()));
+
+    response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+    response.setStatus(200);
+    response.setContentType("application/json");
+
   }
 }
