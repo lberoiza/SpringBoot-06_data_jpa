221f0cf356094a8714ecd4dc061b357899890be2
Luis B <luis.beroiza@adesso.de>
2023-08-23 13:30:39 +0200

    Desabilita el usto de CSRF y de Sesiones en favor de usar JWT

--- src/main/java/com/springboot/app/config/SpringSecurityConfig.java
+++ src/main/java/com/springboot/app/config/SpringSecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -68,6 +70,10 @@ public class SpringSecurityConfig {
         )
         .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
             httpSecurityExceptionHandlingConfigurer.accessDeniedPage(ERROR_403_URL)
+        )
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(httpSecuritySessionManagementConfigurer ->
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
         .logout(LogoutConfigurer::permitAll);
     return http.build();
